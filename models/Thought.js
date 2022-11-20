494e423386ac1dead925276d977c0952cbf4cc55
Corey Skidmore <skidmoreeco@gmail.com>
2022-11-20 15:47:10 -0500

    setting up models & vituals

--- models/Thought.js
+++ models/Thought.js
@@ -1,9 +1,34 @@
 const { Schema, model } = require('mongoose');
 const reactionSchema = require('./Reaction');
 const dateFormat = require('../utils/dateFormat');
+const { stringify } = require('querystring');
 
 const thoughtSchema = new Schema(
- 
+{
+    thoughtText: {
+        type: String,
+        required: true,
+        maxlength: 280,
+        minlength: 1,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    {
+    username: {
+        type: String,
+        required: true,
+    }
+    }
+},
+{
+    toJSON: {
+        getters: true,
+        virtual: true,
+    },
+    id: false,
+}
 );
 
 // Add virtual
--- models/User.js
+++ models/User.js
@@ -1,11 +1,43 @@
 const { Schema, model } = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
-const userSchema = new Schema(
- 
-);
+
+const userSchema = new User.Schema(
+{
+    userName: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        trimmed: true 
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        match: `/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/`
+    },
+    thoughts: {
+        type: User.Schema.Types.ObjectId,
+        ref: 'Thought'
+    },
+    friends: {
+        type: User.Scehma.Types.ObjectId,
+        ref: 'User'
+    },
+},
+{
+    toJSON: {
+        virtuals: true,
+    }
+});
+
+
 
 // Add virtual
 
 // Make model of User
+
+const User = model('user', userSchema);
+userSchema.plugin(uniqueValidator);
 
 module.exports = User;
