3ed963c856b729d02b5a2d697a04b40647b1b53d
Corey Skidmore <skidmoreeco@gmail.com>
2022-11-22 13:30:59 -0500

    working on routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
 const {
+  getThought,
+  getSingletThought,
+  createThought,
+  deleteThought,
+  addUser,
+  removeUser,
   // Exported functions from thought-controller
 } = require('../../controllers/thought-controller');
+
+router.route('/').get(getThought).post(createThought);
+
+router.route('/:thoughtId').get(getSingletThought).delete(deleteThought);
+
+router.route('/thoughts/:thoughtId/reactions')
 
 // Use exported functions from line 3 to create routes
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
 const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
   // Exported functions from user controller
-} = require('../../controllers/user-controller');
+} = require('../../controllers/user-controller.js');
 
+router.route('/').get(getUsers).post(createUser);
 // Use exported functions from line 3 to create routes
+
+router
+  .route('/:userId')
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(deleteUser);
 
 module.exports = router;
